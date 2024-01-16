6df867f7cebd5278f07cc9a80b7dcc3caf2e8ccd
bokarios <abubakr.sg@gmail.com>
2024-01-16 22:40:12 +0200

    adding 404 route

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import HomeView from './views/HomeView'
 import Root from './routes/root'
 import ProductView from './views/ProductView'
+import StoreView from './views/StoreView'
+import NotFoundView from './views/NotFoundView'
 
 function App() {
 	return (
@@ -9,8 +11,10 @@ function App() {
 			<Routes>
 				<Route path="/" element={<Root />}>
 					<Route path="" element={<HomeView />} />
+					<Route path="store" element={<StoreView />} />
 					<Route path="product/:slug" element={<ProductView />} />
 				</Route>
+				<Route path="*" element={<NotFoundView />} />
 			</Routes>
 		</Router>
 	)
